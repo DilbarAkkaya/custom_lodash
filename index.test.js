0fa9c6bc6eb85a85519bf0ad4664c57f03b40a8e
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-22 22:03:59 +0300

    feat: add test arrayisarray for filter function

--- index.test.js
+++ index.test.js
@@ -6,4 +6,7 @@ describe("Filter Function", () => {
   test("Predicate for filter elements > 0 is worked", () => {
     expect(filter([-1, 0, 6, 5, -9], (element) => element > 0)).toStrictEqual([6, 5]);
   });
+  test("Predicate for filter elements > 0 is worked", () => {
+    expect(Array.isArray(filter([-1, 0, 6, 5, -9], (element) => element > 0))).toBe(true);
+  });
 });
