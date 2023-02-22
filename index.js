8d3b2752dc29dc48d06b7235ce4fde8a6f730185
DilbarAkkaya <dilbarmutavalova@gmail.com>
2023-02-22 21:54:36 +0300

    feat: add tests and function for filter

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-function filter() {
-  console.log("0")
+function filter(arr, predicate) {
+let filteredArr = [];
+for (let i = 0; i < arr.length; i++) {
+ if ( predicate(arr[i], i, arr)) {
+  filteredArr = [...filteredArr, arr[i]];
+ }
+}
+return filteredArr;
 }
 export default filter;
--- index.test.js
+++ index.test.js
@@ -3,4 +3,7 @@ describe("Filter Function", () => {
   test("function is defined", () => {
     expect(filter).toBeDefined();
   });
+  test("Predicate for filter elements > 0 is worked", () => {
+    expect(filter([-1, 0, 6, 5, -9], (element) => element > 0)).toStrictEqual([6, 5]);
+  });
 });
